442524dbabf1f2716ed24fe04453b848e20b9c96
Hajir Rasouli <54510090+hazhir95@users.noreply.github.com>
2025-01-05 03:16:01 -0500

    Pass mgr final commit

--- Password_Manager/PassManMain.py
+++ Password_Manager/PassManMain.py
@@ -1,5 +1,8 @@
 import hashlib
 import getpass
+#
+# from Demos.win32ts_logoff_disconnected import username
+# from docutils.parsers.rst.directives import choice
 
 password_manager = {}
 
@@ -8,8 +11,29 @@ def createAccount():
     password = getpass.getpass("Enter your desired password: ")
     hashed_password = hashlib.sha512(password.encode()).hexdigest()
     password_manager[username] = hashed_password
-
-
     print("Account created successfully!")
 
-def
+def login():
+    username = input("Enter your username: ")
+    password = getpass.getpass("Enter your password: ")
+    hashed_password = hashlib.sha512(password.encode()).hexdigest()
+    if username in password_manager.keys() and password_manager[username] == hashed_password:
+        print("Login successful!")
+    else:
+        print("Invalid username or password.")
+
+def main():
+    while True:
+        choice = input("Enter 1 to create an account, 2 to login, or 0 to exit: ")
+        match choice:
+            case "1":
+                createAccount()
+            case "2":
+                login()
+            case "0":
+                break
+            case _:
+                print("Invalid choice.")
+
+if __name__ == "__main__": #running "main" function first
+    main()
